050290642197ccf24977a97d27e3265679e234f0
Mat70x7 <mmoxam@hotmail.com>
2019-01-30 11:28:18 -0500

    Create wireguard_install.sh

---
+++ wireguard_install.sh
@@ -0,0 +1,3 @@
+curl -Lo /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
+yum -y install epel-release wireguard-dkms wireguard-tools
+shutdown now
