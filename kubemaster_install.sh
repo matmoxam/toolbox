9de16de8715da640285ab75d85e33835cccbd1bb
Mat70x7 <mmoxam@hotmail.com>
2019-02-03 08:58:22 -0500

    Update kubemaster_install.sh

--- kubemaster_install.sh
+++ kubemaster_install.sh
@@ -53,6 +53,8 @@ chown $(id -u):$(id -g) $HOME/.kube/config
 
 # Install overlay network in this case flannel
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
+#kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+#KUBECONFIG=/etc/kubernetes/admin.conf kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
 
 kubectl get nodes
 
