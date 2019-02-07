aea22c58f5274ad4d179b67fa3885d503839da24
Mat70x7 <mmoxam@hotmail.com>
2019-02-07 13:49:33 -0500

    Update kubemaster_install.sh

--- kubemaster_install.sh
+++ kubemaster_install.sh
@@ -17,7 +17,7 @@ swapoff -a
 #yum -y install docker net-tools nano
 yum -y install net-tools nano yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum -y install docker-ce
+yum -y install docker-ce-18.06.1.ce-3.el7
 systemctl enable docker
 systemctl start docker
 
