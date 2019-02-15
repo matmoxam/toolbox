88a4e15f12aa69be99576fabfbbf568d853edd32
Mat70x7 <mmoxam@hotmail.com>
2019-02-15 14:03:26 -0500

    Update wireguard_client_setup.sh

--- wireguard/wireguard_client_setup.sh
+++ wireguard/wireguard_client_setup.sh
@@ -7,9 +7,7 @@ fi
 
 # Install all needed binaries for centos7
 curl -Lo /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
-#yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-#yum -y install epel-release dkms wireguard-dkms wireguard-tools
-yum -y install wget
+yum -y install epel-release nano wget gcc
 
 mkdir /opt/wireguard
 cd /opt/wireguard
@@ -17,7 +15,6 @@ cd /opt/wireguard
 KERNEL_VER="$(uname -r)"
 wget http://ftp.riken.jp/Linux/scientific/7.0/x86_64/updates/security/kernel-devel-$KERNEL_VER.rpm
 wget http://ftp.riken.jp/Linux/scientific/7.0/x86_64/updates/security/kernel-headers-$KERNEL_VER.rpm
-yum -y install epel-release nano wget gcc
 yum -y install libmnl-devel elfutils-libelf-devel pkg-config @development-tools
 yum -y install kernel-devel-$KERNEL_VER.rpm
 yum -y install kernel-headers-$KERNEL_VER.rpm
@@ -36,14 +33,5 @@ yum -y install openresolv traceroute
 
 systemctl daemon-reload
 
-#cp wg0.conf /etc/wireguard/wg0.conf
-systemctl enable wg-quick@wg0.service
-mkdir /etc/wireguard
-     
-if [ -f /etc/wireguard/wg0.conf ]; then  
-  # Auto start and enable wiregaurd VPN server
-  systemctl start wg-quick@wg0.service  
-else
-   echo "/etc/wireguard/wg0.conf does not exist. Get this file from the VPN server..."
-   exit 1
-fi
+mkdir /etc/wireguard   
+
