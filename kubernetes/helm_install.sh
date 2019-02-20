97c867237d143319e51a996c542c9900194d635e
Mat70x7 <mmoxam@hotmail.com>
2019-02-20 13:39:55 -0500

    Create helm_install.sh

---
+++ kubernetes/helm_install.sh
@@ -0,0 +1,5 @@
+# Have to setup rbac for tiller after v1.7
+kubectl create serviceaccount -n kube-system tiller
+kubectl create clusterrolebinding tiller-binding --clusterrole=cluster-admin --serviceaccount kube-system:tiller
+helm init --service-account tiller
+
