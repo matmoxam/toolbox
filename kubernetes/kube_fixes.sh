ca58834c38030cb7250d5e5b74021c1e103caf2e
Mat70x7 <mmoxam@hotmail.com>
2019-03-04 09:24:59 -0500

    Update kube_fixes.sh

--- kubernetes/kube_fixes.sh
+++ kubernetes/kube_fixes.sh
@@ -1,2 +1,5 @@
 # Fx for core DNS constant restarts
 kubectl -n kube-system get deployment coredns -o yaml | sed 's/allowPrivilegeEscalation: false/allowPrivilegeEscalation: true/g' | kubectl apply -f -
+
+# Busy Box for debugging
+kubectl run -it --rm --restart=Never busybox --image=busybox sh
